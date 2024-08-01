e8b0b8f21169f88a9d0f277d3a1ea948463b174a
yagmur <96817950+yagmuxr@users.noreply.github.com>
2024-08-01 14:16:02 +0300

    Add files via upload

---
+++ src/test/java/com/example/social_login/SocialLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.social_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
